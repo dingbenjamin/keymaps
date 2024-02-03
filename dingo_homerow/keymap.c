34cd60b1541e7aa94287c71113254672efa02aef
Benjamin Ding <benjamin.ding@imc.com>
2024-02-03 14:44:54 -0600

    Make dingo variant with home row mods

--- dingo_homerow/keymap.c
+++ dingo_homerow/keymap.c
@@ -9,14 +9,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[1] = LAYOUT_gergo(
             KC_TRNS ,KC_EXLM  ,KC_AT          ,KC_LCBR        ,KC_RCBR        ,KC_PIPE  ,                                                  KC_TRNS  ,KC_TRNS        ,KC_TRNS        ,KC_TRNS        ,KC_TRNS  ,KC_BSLS  , 
-            KC_TRNkS,KC_HASH  ,RCTL_T(KC_DLR) ,RALT_T(KC_LPRN),RGUI_T(KC_RPRN),KC_GRV   ,KC_TRNS  ,                         KC_TRNS       ,KC_PLUS  ,LGUI_T(KC_MINS),LALT_T(KC_SLSH),LCTL_T(KC_ASTR),KC_PERC  ,KC_QUOT  , 
+            KC_TRNS ,KC_HASH  ,RCTL_T(KC_DLR) ,RALT_T(KC_LPRN),RGUI_T(KC_RPRN),KC_GRV   ,KC_TRNS  ,                         KC_TRNS       ,KC_PLUS  ,LGUI_T(KC_MINS),LALT_T(KC_SLSH),LCTL_T(KC_ASTR),KC_PERC  ,KC_QUOT  , 
             KC_TRNS ,KC_PERC  ,KC_CIRC        ,KC_LBRC        ,KC_RBRC        ,KC_TILD  ,KC_TRNS  ,KC_PGUP       ,KC_PGDN  ,KC_TRNS       ,KC_AMPR  ,KC_EQL         ,KC_COMM        ,KC_DOT         ,KC_SLSH  ,KC_TRNS  , 
-            KC_INS  ,KC_TRNS  ,KC_TRNS        ,KC_TRNS        ,                                                                                                     ,KC_TRNS        ,KC_TRNS        ,KC_TRNS  ,KC_DEL   ),
+            KC_INS  ,KC_TRNS  ,KC_TRNS        ,KC_TRNS        ,                                                                                                      KC_TRNS        ,KC_TRNS        ,KC_TRNS  ,KC_DEL   ),
 
 	[2] = LAYOUT_gergo(
             KC_TRNS ,KC_1     ,KC_2           ,KC_3           ,KC_4           ,KC_5     ,                                                  KC_6     ,KC_7           ,KC_8           ,KC_9           ,KC_0     ,KC_TRNS  , 
             KC_TRNS ,KC_F1    ,RCTL_T(KC_F2)  ,RALT_T(KC_F3)  ,RGUI_T(KC_F4)  ,KC_F5    ,KC_F6    ,                         KC_TRNS       ,KC_LEFT  ,LGUI_T(KC_DOWN),LALT_T(KC_UP)  ,LCTL_T(KC_RGHT),KC_VOLD  ,KC_VOLU  , 
-            KC_TRNS ,KC_F7    ,KC_F8          ,KC_F9          ,KC_F10         ,KC_F11   ,KC_F12   ,KC_PSCR       ,KC_SLCK  ,KC_TRNS       ,KC_MS_L  ,KC_MS_D        ,KC_MS_U        ,KC_MS_R        ,KC_MPLY  ,KC_TRNS  , 
+            KC_TRNS ,KC_F7    ,KC_F8          ,KC_F9          ,KC_F10         ,KC_F11   ,KC_F12   ,KC_PSCR       ,KC_SCRL  ,KC_TRNS       ,KC_MS_L  ,KC_MS_D        ,KC_MS_U        ,KC_MS_R        ,KC_MPLY  ,KC_TRNS  , 
             KC_TRNS ,KC_TRNS  ,KC_TAB         ,KC_TRNS        ,                                                                                                      KC_TRNS        ,KC_TRNS        ,KC_TRNS  ,KC_TRNS  )
 };
 
