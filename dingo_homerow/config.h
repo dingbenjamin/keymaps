7b838b87857967047081073bd30460847f0df96a
Benjamin Ding <benjamin.ding@imc.com>
2024-02-08 22:44:31 -0600

    Decrease dingo_homerow tap term to 150

--- dingo_homerow/config.h
+++ dingo_homerow/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 200
+#define TAPPING_TERM 150
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
