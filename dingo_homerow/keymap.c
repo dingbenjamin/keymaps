fa9f20c833c161654de8ca86c9607cc8e2143cde
Benjamin Ding <benjamin.ding@imc.com>
2024-02-08 22:43:22 -0600

    Handle non-standard keycode on dingo_homerow symbol layers

--- dingo_homerow/keymap.c
+++ dingo_homerow/keymap.c
@@ -1,5 +1,10 @@
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+    MT_DLR = SAFE_RANGE,
+    MT_LPRN = SAFE_RANGE
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_gergo(
             KC_NO   ,KC_Q     ,KC_W           ,KC_E           ,KC_R           ,KC_T     ,                                                  KC_Y     ,KC_U           ,KC_I           ,KC_O           ,KC_P     ,KC_NO    , 
@@ -20,10 +25,72 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TRNS ,KC_TRNS  ,KC_TAB         ,KC_TRNS        ,                                                                                                      KC_TRNS        ,KC_TRNS        ,KC_TRNS  ,KC_TRNS  )
 };
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case RCTL_T(KC_DLR):
+            if (record->event.pressed && record->tap.count > 0) {
+                tap_code16(KC_DLR);
+                return false;
+            }
+            break;
+        case RALT_T(KC_LPRN):
+            if (record->event.pressed && record->tap.count > 0) {
+                tap_code16(KC_LPRN);
+                return false;
+            }
+            break;
+        case RGUI_T(KC_RPRN):
+            if (record->event.pressed && record->tap.count > 0) {
+                tap_code16(KC_RPRN);
+                return false;
+            }
+            break;
+        case LGUI_T(KC_MINS):
+            if (record->event.pressed && record->tap.count > 0) {
+                tap_code16(KC_MINS);
+                return false;
+            }
+            break;
+        case LALT_T(KC_SLSH):
+            if (record->event.pressed && record->tap.count > 0) {
+                tap_code16(KC_SLSH);
+                return false;
+            }
+            break;
+        case LCTL_T(KC_ASTR):
+            if (record->event.pressed && record->tap.count > 0) {
+                tap_code16(KC_ASTR);
+                return false;
+            }
+            break;
+        case RCTL_T(KC_F2):
+            if (record->event.pressed && record->tap.count > 0) {
+                tap_code16(KC_F2);
+                return false;
+            }
+            break;
+        case RALT_T(KC_F3):
+            if (record->event.pressed && record->tap.count > 0) {
+                tap_code16(KC_F3);
+                return false;
+            }
+            break;
+        case RGUI_T(KC_F4):
+            if (record->event.pressed && record->tap.count > 0) {
+                tap_code16(KC_F4);
+                return false;
+            }
+            break;
+    }
+    return true;
+};
+
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
+
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 
 };
+
 #endif // defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
 
 
